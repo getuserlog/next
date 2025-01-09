8ae443eb0c94ac6382cf231a144f5e011972ae47
8bytede <info@8byte.de>
2025-01-09 23:46:13 +0100

    init

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var React = require("react");
-var Script = require("next/script");
+import React from "react";
+import Script from "next/script";
 
 const CDN_URL = "https://cdn.getuserlog.com/userlog.js";
 
